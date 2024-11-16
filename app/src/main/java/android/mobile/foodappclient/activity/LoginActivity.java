7600a7c0e9a2d3e6c88fcb097ce9e1adeb77ee36
aitx7270 <aitx7270@co-well.com.vn>
2024-11-16 14:59:11 +0700

    call api login

--- app/src/main/java/android/mobile/foodappclient/activity/LoginActivity.java
+++ app/src/main/java/android/mobile/foodappclient/activity/LoginActivity.java
@@ -50,5 +50,60 @@ public class LoginActivity extends AppCompatActivity {
         user.setEmail(email);
         user.setPassword(pass);
 
+        ApiService.api.login(user).enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user_data = null;
+                if (response.isSuccessful()) {
+                    user_data = response.body();
+                    String role = user_data.getRole();
+                    Log.d("role", "onResponse: " +role);
+//                    if (user_data.getRole() == "admin") {
+//                        Toast.makeText(LoginActivity.this, "Người dùng không có quyền truy cập", Toast.LENGTH_SHORT).show();
+//                    }
+                    Log.d("data : ", "data này :" + user_data);
+                    if (user_data != null) {
+                        String userId = user_data.getUserId();
+                        String phone = String.valueOf(user_data.getPhone());
 
+//                        String phone = String.valueOf(user_data.getPhone());
+//                        Log.d("phone", "phone: "+phone);
+                        Log.d("userId : ", "ID ở đây này :" + userId);
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("myPre", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("userId", userId);
+                        editor.apply();
+
+                        SharedPreferences sharedPreferencesPhone = getSharedPreferences("myPre", MODE_PRIVATE);
+                        SharedPreferences.Editor editorPhone = sharedPreferencesPhone.edit();
+                        editorPhone.putString("phone", phone);
+                        editorPhone.apply();
+
+
+                        String username = user_data.getUsername(); //hiển thị tênn client lên Trang chủ
+                        SharedPreferences sharedPreferences_name = getSharedPreferences("myPre", MODE_PRIVATE);
+                        SharedPreferences.Editor edit = sharedPreferences_name.edit();
+                        edit.putString("userName", username);
+                        edit.apply();
+
+                        String email = user_data.getEmail(); //hiển thị email client lên Trang chủ
+                        SharedPreferences sharedPreferences_mail = getSharedPreferences("myPre", MODE_PRIVATE);
+                        SharedPreferences.Editor edit_mail = sharedPreferences_mail.edit();
+                        edit_mail.putString("mail", email);
+                        edit_mail.apply();
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                }  else {
+                    Toast.makeText(LoginActivity.this, "Người dùng không tồn tại", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
