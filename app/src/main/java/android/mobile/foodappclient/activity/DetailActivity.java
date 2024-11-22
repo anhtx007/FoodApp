2d6e5967d9503be92af045421219c34eabe8b48a
aitx7270 <aitx7270@co-well.com.vn>
2024-11-22 18:12:10 +0700

    hien thi chi tiet sp

--- app/src/main/java/android/mobile/foodappclient/activity/DetailActivity.java
+++ app/src/main/java/android/mobile/foodappclient/activity/DetailActivity.java
@@ -3,6 +3,7 @@ package android.mobile.foodappclient.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.mobile.foodappclient.databinding.ActivityDetailBinding;
+import android.mobile.foodappclient.model.Cart;
 import android.mobile.foodappclient.model.CartResponse;
 import android.mobile.foodappclient.model.WithList;
 import android.mobile.foodappclient.service.ApiService;
@@ -10,6 +11,9 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.mobile.foodappclient.R;
 import android.mobile.foodappclient.model.Product;
--- app/src/main/java/android/mobile/foodappclient/adapter/ListFoodAdapter.java
+++ app/src/main/java/android/mobile/foodappclient/adapter/ListFoodAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.mobile.foodappclient.R;
-import android.mobile.foodappclient.activity.DetailActivity;
 import android.mobile.foodappclient.model.Product;
 import android.util.Log;
 import android.view.LayoutInflater;
